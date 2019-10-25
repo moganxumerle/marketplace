6bdb29b4b8d4a939d55b20a0fe21752579187a51
Mogan Xumerle <moganrz@gmail.com>
2019-10-25 11:24:12 -0300

    Criando padrao de retorno do Payload

---
+++ src/main/java/com/luizalabs/marketplace/model/RetornoModel.java
@@ -0,0 +1,10 @@
+package com.luizalabs.marketplace.model;
+
+import lombok.Data;
+
+@Data
+public class RetornoModel {
+
+	private Object data;
+	
+}
--- src/main/java/com/luizalabs/marketplace/resource/ProductResource.java
+++ src/main/java/com/luizalabs/marketplace/resource/ProductResource.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.luizalabs.marketplace.model.Product;
+import com.luizalabs.marketplace.model.RetornoModel;
 import com.luizalabs.marketplace.service.ProductService;
 
 @RestController
@@ -18,9 +19,12 @@ public class ProductResource {
 	private ProductService prodServ;
 	
 	@PostMapping("/products")
-	public ResponseEntity<List<Product>> create(@RequestBody List<Product> lstProduct) {
+	public ResponseEntity<?> create(@RequestBody List<Product> lstProduct) {
 		
-		return ResponseEntity.ok(prodServ.save(lstProduct));
+		RetornoModel ret = new RetornoModel();
+		ret.setData(prodServ.save(lstProduct));
+		
+		return ResponseEntity.ok(ret);
 		
 	}
 	
